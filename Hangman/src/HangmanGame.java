adf146a60a2b91e8010223323895f5a36aef5c0c
Ben <56470173+btpokin@users.noreply.github.com>
2020-12-03 16:41:59 +0900

    Add files via upload

---
+++ Hangman/src/HangmanGame.java
@@ -0,0 +1,15 @@
+/**
+ * All functionality for the hangman game included here.
+ * Will draw upon other packages and classes to run the game correctly
+ * @author ben
+ *
+ */
+public class HangmanGame {
+
+	public static void main(String[] args) {
+		System.out.println("Crickets chirping.");
+		System.out.println("No game here yet, bub.");
+
+	}
+
+}
