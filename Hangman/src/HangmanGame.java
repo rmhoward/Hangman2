d49611d091a3a46120fbb55640eb2a16c85a1822
Christian <Christian@DESKTOP-JUFM6E7.attlocal.net>
2020-12-03 22:18:53 -0500

    First iteration of office hours code

--- Hangman/src/HangmanGame.java
+++ Hangman/src/HangmanGame.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList
+
 /**
  * All functionality for the hangman game included here.
  * Will draw upon other packages and classes to run the game correctly
@@ -6,6 +8,29 @@
  */
 public class HangmanGame {
 
+	/**
+	 * Play Hangman with a dictionary file of words
+	 * with the given fileName.
+	 * @param fileName for the dictionary file
+	 */
+	private void playGame(String fileName) {
+		
+		//create hangman directory
+		ArrayList<String> wordList = HangmanDictionaryReader.readDictionary(fileName);
+		
+		boolean gameOver = false;
+		Scanner scanner = new Scanner (System.in);
+		
+		while (!gameOver) {
+			
+			Hangman hangman = this.getHanmanVersion(wordlist);
+			
+			System.out.println("Play again? Y/N");
+		}
+		
+	}
+	
+	
 	public static void main(String[] args) {
 		System.out.println("Crickets chirping.");
 		System.out.println("No game here yet, bub.");
