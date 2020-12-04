8ccf7a45cc9bab15646df07d98d192e7a8ae9ef8
Christian Pearson <lcpearso@umich.edu>
2020-12-04 00:09:01 -0500

    Update HangmanEvil.java

--- Hangman/src/hangman/HangmanEvil.java
+++ Hangman/src/hangman/HangmanEvil.java
@@ -1,10 +1,25 @@
 package hangman;
 
-/**
- * Functionality for hangman evil
- * @author ben
- *
- */
-public class HangmanEvil {
+import java.io.File
 
+/**
+ * Evil version of Hangman
+ * @author 
+ */
+public class HangmanEvil extends Hangman {
+
+	/**
+	 * Create instance of evil hangman with a given list of words.
+	 * @param wordlist list of words
+	 */
+	public HangmanEvil(ArrayList<String> wordList) {
+		this.wordList = wordList;
+	}
+	
+	/**
+	 * Takes a given word and 
+	 * sets the length of the word for Evil Hangman
+	 * @param givenWord to use for the game.
+	 * @return Selected word represented by "_" String characters.
+	 */
 }
